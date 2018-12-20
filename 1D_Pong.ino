e2ce89c1f38f4ba291d7b3314ceb11e27aae946d
mr-rampage <readonly@che.wbac.ca>
2018-12-19 21:35:47 -0500

    feat(light mode)

--- 1D_Pong.ino
+++ 1D_Pong.ino
@@ -12,13 +12,21 @@
 #define TO_PLAYER_1 255
 #define TO_PLAYER_2 1
 
+volatile boolean mode = LOW;
 volatile byte ballLocation = 0;
 volatile byte ballDirection = 1;
 volatile float ballDelay = 250;
 
+volatile byte lightColour = 1;
+volatile byte lightBrightness = 1;
+
+volatile boolean currentButtonState[2] = { HIGH, HIGH };
+volatile boolean previousButtonState[2] = { HIGH, HIGH };
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, LED_DIN,  NEO_GRB + NEO_KHZ800);
 
 void setup() {
+  mode = LOW;
   initializeGameState();
   
   pinMode(PLAYER_1, INPUT);
@@ -26,20 +34,17 @@ void setup() {
 
   pinMode(PLAYER_2, INPUT);
   digitalWrite(PLAYER_2, HIGH);
-  
+
+  Serial.begin(9600);
   strip.begin();
 }
 
 void loop() {
-  if (ballLocation < NUMPIXELS && ballLocation >= 0) {
+  if (mode == HIGH) {
     runGame();
   } else {
-    fill
   }
-
-  if (digitalRead(PLAYER_1) == LOW && digitalRead(PLAYER_2) == LOW) {
-    initializeGameState();
-  }
+  setMode();
 }
 
 void initializeGameState() {
@@ -48,15 +53,46 @@ void initializeGameState() {
   ballDelay = 250;
 }
 
+void setMode() {
+  currentButtonState[0] = digitalRead(PLAYER_1);
+  currentButtonState[1] = digitalRead(PLAYER_2);
+
+  if ((currentButtonState[0] == LOW || currentButtonState[1] == LOW) && 
+      (currentButtonState[0] != previousButtonState[0] ||
+       currentButtonState[1] != previousButtonState[1])) {
+    if (digitalRead(PLAYER_1) == LOW && digitalRead(PLAYER_2) == LOW) {
+      initializeGameState();
+      mode = !mode;    
+    }
+    
+    if (mode == LOW) {
+      if (digitalRead(PLAYER_1) == LOW) {
+        lightBrightness = max(lightBrightness << 1, 1);
+        lightStrip();
+      }
+    
+      if (digitalRead(PLAYER_2) == LOW) {
+        lightColour = max(lightColour << 1, 1);
+        lightStrip();
+      }
+    }
+  }
+
+  previousButtonState[0] = currentButtonState[0];
+  previousButtonState[1] = currentButtonState[1];
+}
+
 void runGame() {
-  clearStrip();
-  updateGameState();
-  renderGameState();
-  delay(ballDelay);
+  if (ballLocation < NUMPIXELS && ballLocation >= 0) {
+    clearStrip();
+    updateGameState();
+    renderGameState();
+    delay(ballDelay);
+  }
 }
 
 void clearStrip() {
-  fillStrip();
+  createPongBoard();
   strip.show();
 }
 
@@ -96,7 +132,7 @@ void setBallDelay() {
   }
 }
 
-void fillStrip() {
+void createPongBoard() {
   for(int i=0;i<NUMPIXELS;i++){  
     if ((i >= 1 && i <= 2) || (i <= NUMPIXELS - 2 && i >= NUMPIXELS - 3)) {
       setPixelColor(i, 1, 1, 0);
@@ -108,6 +144,22 @@ void fillStrip() {
   }
 }
 
-void setPixelColor( uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
+void lightStrip() {
+  for(int i=0;i<NUMPIXELS;i++){  
+    switch (lightColour) {
+      case 1: strip.setPixelColor(i, 0, 0, lightBrightness); break;
+      case 2: strip.setPixelColor(i, 0, lightBrightness, 0); break;
+      case 4: strip.setPixelColor(i, 0, lightBrightness, lightBrightness); break;
+      case 8: strip.setPixelColor(i, lightBrightness, 0, 0); break;
+      case 16: strip.setPixelColor(i, lightBrightness, 0, lightBrightness); break;
+      case 32: strip.setPixelColor(i, lightBrightness, lightBrightness, 0); break;
+      case 64: strip.setPixelColor(i, lightBrightness, lightBrightness, lightBrightness); break;
+      default: lightColour = 1; break; 
+    }
+  }
+  strip.show();
+}
+
+void setPixelColor(uint16_t n, uint8_t r, uint8_t g, uint8_t b) {
    strip.setPixelColor(n, r, g, b);
 }
